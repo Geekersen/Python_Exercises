1acf6184d60fbe77a7ebd8dbf00739375fa033d1
Geekersen <richard-sen@outlook.com>
2017-02-02 13:12:38 +0800

    Initial commit

---
+++ 100_Python_Exercises/add_number_to_image.py
@@ -0,0 +1,25 @@
+"""
+
+add_number_to_image.py
+
+描述：在图片的右上角添加一个数字
+
+"""
+
+from PIL import Image, ImageDraw, ImageFont
+
+
+def add_number_to(image):
+    draw = ImageDraw.Draw(image)
+    number_size = 100
+    number_font = ImageFont.truetype("C:\\Windows\\Fonts\\SourceCodePro-Semibold.ttf", size=number_size)
+    number_color = "#ff0000"
+    width, height = image.size
+    draw.text((width - 150, 0), '99', font=number_font, fill=number_color)
+    image.save("D:\\Code\\testdata\\result.jpg", "jpeg")
+
+    return 0
+
+if __name__ == '__main__':
+    img = Image.open("D:\\Code\\testdata\\test.jpg")
+    add_number_to(img)
---
+++ Test_Program/pyDemo.py
@@ -0,0 +1,12 @@
+class Word:
+    def __init__(self, text):
+        self.text = text
+
+    def __eq__(self, word2):
+        return self.text.lower() == word2.text.lower()
+
+first = Word('ha')
+second = Word('Ha')
+third = Word('eh')
+print(first == second)
+print(first == third)
