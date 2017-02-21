9eaf6c6764d245eaee00a523766f21787a579732
Geekersen <richard-sen@outlook.com>
2017-02-21 19:52:34 +0800

    add exercise 010-get_urls_from_html.py

---
+++ 100_Python_Exercises/010-get_urls_from_html.py
@@ -0,0 +1,24 @@
+"""
+
+010-get_urls_from_html.py
+
+描述：得到网站上的所有网络链接
+
+"""
+from pprint import pprint
+from bs4 import BeautifulSoup
+import urllib.request
+
+
+def get_urls_from(url):
+    with urllib.request.urlopen(url) as html:
+        page = html.read()
+
+    links = []
+    soup = BeautifulSoup(page, 'lxml')
+    for link in soup.find_all('a'):
+        links.append(link.get('href'))
+    return links
+
+if __name__ == '__main__':
+    pprint(get_urls_from('http://stackoverflow.com/questions/24075855/python-and-collective-intelligence-keyerror-href'))
